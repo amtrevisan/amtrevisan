4e9887eda058166e435c0ae53c1b85c672b4c5f0
AlexMoralesDev <alex.morales8@upr.edu>
2025-12-01 14:00:04 -0400

    Fix ClickSpark positioning as fixed overlay

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -14,23 +14,28 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <ClickSpark
-        sparkColor="#fbbf24"
-        sparkSize={8}
-        sparkRadius={30}
-        sparkCount={8}
-        duration={600}
-        easing="ease-out"
-        extraScale={1.2}
-      >
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </ClickSpark>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* Click Spark Overlay */}
+      <div className="fixed inset-0 pointer-events-none" style={{ zIndex: 9999 }}>
+        <ClickSpark
+          sparkColor="#fbbf24"
+          sparkSize={8}
+          sparkRadius={30}
+          sparkCount={8}
+          duration={600}
+          easing="ease-out"
+          extraScale={1.2}
+        >
+          <div className="w-screen h-screen" />
+        </ClickSpark>
+      </div>
     </TooltipProvider>
   </QueryClientProvider>
 );
